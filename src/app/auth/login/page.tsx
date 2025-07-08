32eab38e4d82a9fecc92d872bac3f30c7542e71b
phea <rith.sophea@institute.pse.ngo>
2025-07-08 15:17:28 +0700

    login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,42 +1,118 @@
+"use client";
+
 export default function Login() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        <form>
-          <div className="mb-4">
-            <label className="block text-sm font-medium mb-2" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your email"
-            />
+    <>
+      <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 px-6">
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-5 shadow-2xl sm:rounded-xl sm:px-10">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+              <img
+                className="mx-auto h-10 w-auto"
+                src="https://www.svgrepo.com/show/301692/login.svg"
+                alt="Workflow"
+              />
+              <h2 className="mt-9 text-center text-3xl leading-9 font-extrabold text-gray-900">
+                Sign in to your account
+              </h2>
+            </div>
+            <form>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-5  text-gray-700 mt-7"
+                >
+                  Email address
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm text-black">
+                  <input
+                    id="email"
+                    name="email"
+                    placeholder="user@example.com"
+                    type="email"
+                    defaultValue=""
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                  <div className="hidden absolute inset-y-0 right-0 pr-3 items-center pointer-events-none">
+                    <svg
+                      className="h-5 w-5 text-red-500"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-6">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-5 text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1 rounded-md shadow-sm text-black">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                </div>
+              </div>
+              <div className="mt-6 flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember_me"
+                    name="remember"
+                    type="checkbox"
+                    defaultValue={1}
+                    className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                  />
+                  <label
+                    htmlFor="remember_me"
+                    className="ml-2 block text-sm leading-5 text-gray-900"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <div className="text-sm leading-5">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-500 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-6">
+                <span className="block w-full rounded-md shadow-sm">
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                  >
+                    Sign in
+                  </button>
+                </span>
+                <p className="text-center text-sm text-gray-500 mt-7">
+                  Don't have an account yet?
+                  <a
+                    href="#!"
+                    className="font-semibold text-gray-600 hover:underline focus:text-gray-800 focus:outline-none"
+                  >
+                    Sign up
+                  </a>
+                  .
+                </p>
+              </div>
+            </form>
           </div>
-          <div className="mb-6">
-            <label
-              className="block text-sm font-medium mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your password"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition duration-200"
-          >
-            Login
-          </button>
-        </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
